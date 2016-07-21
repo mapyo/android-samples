709801aed9e9f62e041172aac8c93f6f0155516b
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-07-21 09:28:53 +0900

    Create FabSpeedDial sample

--- FabSpeedDial/app/src/main/java/com/mapyo/fabspeeddial/MainActivity.java
+++ FabSpeedDial/app/src/main/java/com/mapyo/fabspeeddial/MainActivity.java
@@ -1,52 +1,69 @@
 package com.mapyo.fabspeeddial;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.fab_main)
+    View fabMain;
+
+    @BindView(R.id.background)
+    View background;
+
+    @BindView(R.id.fab1)
+    View fab1;
+
+    @BindView(R.id.fab2)
+    View fab2;
+
+    private Animation rotateForward, rotateBackward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        rotateForward = AnimationUtils.loadAnimation(this, R.anim.fab_rotate_forward_animation);
+        rotateBackward = AnimationUtils.loadAnimation(this, R.anim.fab_rotate_backward_animation);
+
+        closeFab();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @OnClick(R.id.fab_main)
+    void clickFab() {
+        if (background.getVisibility() == View.VISIBLE) {
+            closeFab();
+        } else {
+            openFab();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    void openFab() {
+        background.setVisibility(View.VISIBLE);
+        fab1.setVisibility(View.VISIBLE);
+        fab2.setVisibility(View.VISIBLE);
+        fabMain.startAnimation(rotateForward);
+    }
+
+    void closeFab() {
+        background.setVisibility(View.GONE);
+        fab1.setVisibility(View.GONE);
+        fab2.setVisibility(View.GONE);
+        fabMain.startAnimation(rotateBackward);
     }
 }
