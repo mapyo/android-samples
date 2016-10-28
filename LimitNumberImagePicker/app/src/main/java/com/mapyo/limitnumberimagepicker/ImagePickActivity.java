c1c7a2a277b027994808b763614972e43679109b
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-10-28 17:31:16 +0900

    Create ImagePickActivity

---
+++ LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/ImagePickActivity.java
@@ -0,0 +1,13 @@
+package com.mapyo.limitnumberimagepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagePickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_pick);
+    }
+}
