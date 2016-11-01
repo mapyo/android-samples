c5fae628971d39ec32baa07420f695a8add148b7
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-11-01 16:24:17 +0900

    cursorの部分を勘違いしていた

--- LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/ImagePickActivity.java
+++ LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/ImagePickActivity.java
@@ -12,7 +12,6 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.mapyo.limitnumberimagepicker.databinding.ActivityImagePickBinding;
 import com.squareup.picasso.Picasso;
@@ -53,17 +52,18 @@ public class ImagePickActivity extends AppCompatActivity implements LoaderManage
     }
 
     private void showImages(Cursor cursor) {
+        int i = 0;
         cursor.moveToFirst();
         do {
-            for (int i = 0; i < cursor.getColumnCount(); i++) {
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                long id = cursor.getLong(fieldIndex);
-                Log.d(getClass().getSimpleName(), cursor.getColumnName(i) + " : " + cursor.getString(i));
-                if (i==0) {
-                    Uri uri = ContentUris.withAppendedId( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-                    setImage(uri);
-                }
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            long id = cursor.getLong(fieldIndex);
+            Uri uri = ContentUris.withAppendedId( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            L.d(uri.toString());
+            if (i == 1) {
+                setImage(uri);
             }
+
+            i++;
         } while (cursor.moveToNext());
 
         cursor.close();
---
+++ LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/L.java
@@ -0,0 +1,29 @@
+package com.mapyo.limitnumberimagepicker;
+
+import android.util.Log;
+
+import java.util.regex.Pattern;
+
+public class L {
+    /**
+     * デバック用ログを出力する。 本番リリース時は出力されない。
+     *
+     * @param msg 出力するメッセージ
+     */
+    public static void d(String msg) {
+        if (!BuildConfig.DEBUG) return;
+        Log.d(getTag(), msg);
+    }
+
+    private static String getTag() {
+        final StackTraceElement trace = Thread.currentThread().getStackTrace()[4];
+        final String cla = trace.getClassName();
+        Pattern pattern = Pattern.compile("[\\.]+");
+        final String[] splitedStr = pattern.split(cla);
+        final String simpleClass = splitedStr[splitedStr.length - 1];
+        final String mthd = trace.getMethodName();
+        final int line = trace.getLineNumber();
+        return simpleClass + "#" + mthd + ":" + line;
+    }
+}
+
