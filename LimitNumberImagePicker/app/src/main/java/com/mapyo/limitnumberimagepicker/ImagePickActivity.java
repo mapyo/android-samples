3d9a2111b23542a538879ef86ed093f27aed425c
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-11-01 14:35:21 +0900

    画像を１つだけ表示させるところまで書いた

--- LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/ImagePickActivity.java
+++ LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/ImagePickActivity.java
@@ -1,17 +1,70 @@
 package com.mapyo.limitnumberimagepicker;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class ImagePickActivity extends AppCompatActivity {
+import com.mapyo.limitnumberimagepicker.databinding.ActivityImagePickBinding;
+import com.squareup.picasso.Picasso;
+
+public class ImagePickActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String LIMIT_NUMBER_IMAGE = "limit_number_image";
+    private static final int FETCH_IMAGE_LIST_LOADER_ID = 1;
+    private static final String[] PROJECTION = {MediaStore.MediaColumns.DATA};
+
+    private static final String ORDER_BY = MediaStore.Images.Media._ID + " DESC";
+    private ActivityImagePickBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_pick);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_image_pick);
+
+        // 画像一覧をとってくる
+        // cursorを使う
+        getSupportLoaderManager().initLoader(FETCH_IMAGE_LIST_LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(ImagePickActivity.this, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, ORDER_BY);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        do {
+            for (int i = 0; i < cursor.getColumnCount(); i++) {
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                long id = cursor.getLong(fieldIndex);
+                Log.d(getClass().getSimpleName(), cursor.getColumnName(i) + " : " + cursor.getString(i));
+                if (i==0) {
+                    Uri uri = ContentUris.withAppendedId( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                    setImage(uri);
+                }
+            }
+        } while (cursor.moveToNext());
+    }
+
+    private void setImage(Uri uri) {
+        Picasso.with(this)
+                .load(uri)
+                .into(binding.image);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 
     public static class IntentBuilder {
