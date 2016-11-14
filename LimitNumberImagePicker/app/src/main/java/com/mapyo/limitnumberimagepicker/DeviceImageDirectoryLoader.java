dce42bf1aae88489dbaa7c276cae57421d08aaf9
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-11-14 12:17:16 +0900

    Add DeviceImageDirectoryLoader

--- LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/DeviceImageDirectoryLoader.java
+++ LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/DeviceImageDirectoryLoader.java
@@ -1,7 +1,15 @@
 package com.mapyo.limitnumberimagepicker;
 
-/**
- * Created by usr0600285 on 2016/11/14.
- */
-public class DeviceImageDirectoryLoader {
+import android.content.Context;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+
+public class DeviceImageDirectoryLoader extends CursorLoader {
+    private static final String[] PROJECTION = { MediaStore.Images.Media.BUCKET_ID, MediaStore.Images.Media.BUCKET_DISPLAY_NAME, MediaStore.Images.Media._ID };
+    private static final String BUCKET_GROUP_BY = "1) GROUP BY 1,(2";
+    private static final String BUCKET_ORDER_BY = "MAX(datetaken) DESC";
+
+    public DeviceImageDirectoryLoader(Context context) {
+        super(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, PROJECTION,  BUCKET_GROUP_BY, null, BUCKET_ORDER_BY);
+    }
 }
