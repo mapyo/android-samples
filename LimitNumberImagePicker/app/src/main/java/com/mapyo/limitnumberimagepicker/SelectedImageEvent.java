76b5d125638c18604988b8f60f906e934ff68193
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-11-14 21:54:52 +0900

    Add SelectedImageEvent

--- LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/SelectedImageEvent.java
+++ LimitNumberImagePicker/app/src/main/java/com/mapyo/limitnumberimagepicker/SelectedImageEvent.java
@@ -1,4 +1,17 @@
 package com.mapyo.limitnumberimagepicker;
 
+import android.net.Uri;
+
+import java.util.List;
+
 public class SelectedImageEvent {
+    private List<Uri> pickImageUriList;
+
+    public SelectedImageEvent(List<Uri> pickImageUriList) {
+        this.pickImageUriList = pickImageUriList;
+    }
+
+    public List<Uri> getPickImageUriList() {
+        return pickImageUriList;
+    }
 }
